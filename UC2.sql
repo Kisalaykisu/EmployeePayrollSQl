61c1264215752b120e0592aebe8fcf73f4097f07
Kisalay <kisalaykisu@gmail.com>
2022-07-21 18:14:34 +0530

    Update UC2.sql

--- UC2.sql
+++ UC2.sql
@@ -1,5 +1,12 @@
 ## UC2
 
+SHOW DATABASES;                   #TO see all Databases
+#UC1
+CREATE DATABASE payroll_service;   #create database
+USE payroll_service;               # To use payroll_service Database
+SELECT DATABASE();                 # To see current database
+
+
  CREATE TABLE employee_payroll    #create table
     (
     id  INT unsigned NOT NULL AUTO_INCREMENT,    #Unique ID for the record
