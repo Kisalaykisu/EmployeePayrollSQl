53483ce05034d1f40288f6d83ebdeded2eca27c9
Kisalay <kisalaykisu@gmail.com>
2022-07-23 21:31:54 +0530

    Update UC6_AlterTableAddGender.sql

--- UC6_AlterTableAddGender.sql
+++ UC6_AlterTableAddGender.sql
@@ -1,3 +1,4 @@
+##################################################################################################################
 #UC6
 
 ALTER TABLE employee_payroll ADD gender CHAR(1) AFTER name;       #Adding gender field
@@ -6,4 +7,6 @@ ALTER TABLE employee_payroll ADD gender CHAR(1) AFTER name;       #Adding gender
 update employee_payroll set gender='F' where name = 'Bill';
 update employee_payroll set gender='M' where name = 'Kisalay' or name = 'Charlie';
 update employee_payroll set salary = 300000000.00  where name = 'Kisalay';
+
+#######################################################################################################################
 
