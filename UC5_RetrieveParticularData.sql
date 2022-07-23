48cf938c02a489f2330808bb44f0a3495bce70ad
Kisalay <kisalaykisu@gmail.com>
2022-07-23 20:45:34 +0530

    Update UC5_RetrieveParticularData.sql

--- UC5_RetrieveParticularData.sql
+++ UC5_RetrieveParticularData.sql
@@ -1,6 +1,8 @@
+###########################################################################################################
 #UC5
 
 SELECT salary FROM employee_payroll WHERE name = 'Bill';      #SELECTING Bill entry
 
 SELECT * FROM employee_payroll                                          #Select Employee from Date Range
      WHERE start BETWEEN CAST('2018-01-01' AS DATE) AND DATE(NOW());
+################################################################################################################
